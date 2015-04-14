1e3b0bf309116d66b279436a860fe1bcc24b7436
shenxiul <shenxiu@stanford.edu>
2015-04-14 05:17:31 +0000

    plotTrend updated

--- src/plotTrend.py
+++ src/plotTrend.py
@@ -1,17 +1,38 @@
 import sys
 import scipy.sparse
 from sparseIO import *
+from lookUpTable import *
 
-def queryInfo(appname, metric):
-    pass
+def queryId(cursor, appname):
+    query = 'SELECT id FROM Products WHERE name = "%s"' % appname
+    cursor.execute(query)
+    return [a for (a,) in list(cursor.fetchall())]
+
+def queryCategory(cursor, product_id):
+    query = 'SELECT category,idx FROM Product_category_lookup WHERE id = "%s"' % product_id
+    cursor.execute(query)
+    return cursor.fetchall()[0]
+
+def queryInfo(cursor, appname, metric):
+    id_list = queryId(cursor, appname)
+    category_idx_list = [queryCategory(cursor, id) for id in id_list]
+    print id_list, category_idx_list
 
 if __name__ == '__main__':
-    username = sys.argv[0]
-    password = sys.argv[1]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    connection, cursor = setup_connection(username, password)
     while(True):
         print "App name: "
         appname = sys.stdin.readline()
-        if appname == : exit(0)
+        if appname == "":
+            close_connection(connection, cursor)
+            exit(0)
+        else: appname = appname[:-1]
         print "Metric: "
         metric = sys.stdin.readline()
-        queryInfo(appname, metric)
+        if metric == "":
+            close_connection(connection, cursor)
+            exit(0)
+        else: metric = int(metric)
+        queryInfo(cursor, appname, metric)

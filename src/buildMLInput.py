18e51fe339e408aff1783e32a267c0e91b2c8d74
Shenxiu Liu <shenxiu@stanford.edu>
2015-04-26 15:43:56 -0700

    normalization and new label methods

--- src/buildMLInput.py
+++ src/buildMLInput.py
@@ -15,9 +15,11 @@ import random
 #   successThreshold: win from category have to win how many windows.      #
 #   garbageThreshold: total download threshold for candidate               #
 #   testPortion: the portion of data treating as test                      #
+#   top: top k success                                                     #
 # Note: successThreshold <= predictTimeWindow - slidingWindowsize + 1      #
 ############################################################################
 WEEK = 7 # don't change this, you know why :-)
+EPSILON = 0.000001
 inputFile = './1/Productivity/datamatrix_metric_1.npz'
 predictTimeWindow = 10
 featureTimeWindow = 10
@@ -26,6 +28,7 @@ outOfSigmaSuccess = 1
 successThreshold = 5
 garbageThreshold = featureTimeWindow * WEEK # a download a day, keep doctors away.
 testPortion = 0.2
+top = 100
 
 def rawDataMatrix(inputFile):
     """
@@ -59,7 +62,7 @@ def compressMatrix(rawData, windowSize=None, skipDay=WEEK):
         slidingWindowMatrix[:,i] = rawData[:,(WEEK * i):(WEEK * i + windowSize)].sum(1).T
     return sps.csr_matrix(slidingWindowMatrix)
 
-def swipeOutInactiveApp(downloadMatrix, predictMatrix, leastDownload=None):
+def swipeOutInactiveApp(downloadMatrix, predictMatrix = None, leastDownload=None):
     """
     Function: swipeOutInactiveApp
         Cleaning Apps with too small download amount in feature matrix.
@@ -73,7 +76,10 @@ def swipeOutInactiveApp(downloadMatrix, predictMatrix, leastDownload=None):
     if leastDownload is None: leastDownload = garbageThreshold
     totalDownload = np.array(downloadMatrix.sum(1)).ravel()
     candidate = np.where(totalDownload > leastDownload)[0]
-    return downloadMatrix[candidate,:].toarray(), predictMatrix[candidate,:].toarray(), candidate[:,None]
+    if predictMatrix is not None:
+        return downloadMatrix[candidate,:].toarray(), predictMatrix[candidate,:].toarray(), candidate[:,None]
+    else:
+        return downloadMatrix[candidate,:].toarray(), None, candidate[:,None]
 
 def generateAccumulateLabelByCol(dataMatrix, numberSigma=None):
     """
@@ -91,6 +97,23 @@ def generateAccumulateLabelByCol(dataMatrix, numberSigma=None):
     # standard deviation
     std = dataMatrix.std(0)
     return dataMatrix > (mean + std * numberSigma)
+
+def generateTopkLabelByCol(dataMatrix, k=None):
+    """
+    Function: generateTopkLabelByCol
+        Generate top-k label for each column.
+    Input:
+        dataMatrix: data to generate label, type numpy array.
+        k: top rank.
+    Output:
+        matrix as same shape with dataMatrix, elements are 0 or 1.
+    """
+    if k is None: k = top
+    label = np.zeros(dataMatrix.shape)
+    topkIndex = np.argpartition(dataMatrix, dataMatrix.shape[0] - k, axis=0)[-k:,:]
+    for colIdx in xrange(topkIndex.shape[1]):
+        label[topkIndex[:,colIdx], colIdx] = 1
+    return label
 
 def standardize(featureMatrix):
     """
@@ -117,7 +140,7 @@ def normalize(featureMatrix):
     """
     return featureMatrix / featureMatrix.sum(0)
 
-def sample(dataSet, portionOfTestSet=None, seed=40):
+def sample(dataSet, seed, portionOfTestSet=None):
     """
     Function: sample
         split whole dataset into training set and test set.
@@ -143,7 +166,7 @@ def sample(dataSet, portionOfTestSet=None, seed=40):
     return train, test
 
 def singlePredictTime(totalDataMatrix, predictTimeStamp, windowSize=None,
-        featureSize=None, predictSize=None, success=None):
+        featureSize=None, predictSize=None, success=None, numberOfSigma=None):
     """
     Function: singlePredictTime
         Give a predict time, generate standardized features and labels.
@@ -154,22 +177,23 @@ def singlePredictTime(totalDataMatrix, predictTimeStamp, windowSize=None,
         featureSize: feature dimension.
         predictSize: prediction dimension to generate label.
         success: win at least these sliding windows to label as successful.
+        numberOfSigma: outOfSigmaSuccess
     Output:
-        feature matrix, accumulate label, sliding window label, prediction window.
-    Application note: In this function, swipeOutInactiveApp(...) and
-    generateAccumulateLabelByCol(...) are called with default parameters.
+        feature matrix, accumulate label, sliding window label, prediction window and corresponding index.
+    Application note: In this function, swipeOutInactiveApp(...) is called with default parameters.
     """
     if windowSize is None: windowSize = slidingWindowSize
     if featureSize is None: featureSize = featureTimeWindow - slidingWindowSize + 1
     if predictSize is None: predictSize = predictTimeWindow - slidingWindowSize + 1
     if success is None: success = successThreshold
+    if numberOfSigma is None: numberOfSigma = outOfSigmaSuccess
     featureEndTime = predictTimeStamp - windowSize + 1
     featureStartTime = featureEndTime - featureSize
     featureTotal = totalDataMatrix[:,featureStartTime:featureEndTime]
     predictTotal = totalDataMatrix[:,predictTimeStamp:predictTimeStamp + predictSize]
     featureMatrix, predictMatrix, remainingIndex = swipeOutInactiveApp(featureTotal, predictTotal)
-    accumulateLabel = generateAccumulateLabelByCol(predictMatrix.sum(1))
-    eachWindowLabel = generateAccumulateLabelByCol(predictMatrix)
+    accumulateLabel = generateAccumulateLabelByCol(predictMatrix.sum(1), numberOfSigma)
+    eachWindowLabel = generateAccumulateLabelByCol(predictMatrix, numberOfSigma)
     slidingWindowLabel = (eachWindowLabel.sum(1) >= success)
     predictTimeCol = np.ones(remainingIndex.shape[0], dtype='int')[:,None]
     return (standardize(featureMatrix),
@@ -178,20 +202,70 @@ def singlePredictTime(totalDataMatrix, predictTimeStamp, windowSize=None,
             standardize(predictMatrix),
             np.hstack((remainingIndex, predictTimeCol)))
 
-def generateFeatureMatrixAndLabel(totalDataMatrix, windowSize=None,
-        featureWindow=None, predictWindow=None, success=None):
+def singlePredictTimeNew(totalDataMatrix, predictTimeStamp, standardizeMethod=None,
+        windowSize=None, featureSize=None, predictSize=None,
+        failTime=2, successTime=2, topk=None):
+    """
+    Function: singlePredictTimeNew
+        Give a predict time, generate features and labels.
+    Input:
+        totalDataMatrix: the complete data matrix.
+        predictTimeStamp: time to start prediction.
+        standardizeMethod: method for standardization.
+        windowSize: size of sliding window.
+        featureSize: feature dimension.
+        predictSize: prediction dimension to generate label.
+        failTime: Fail as least first several windows to be considered as incremental.
+        successTime: Fail as least last several windows to be considered as incremental.
+        topk: Top k to be considered as good.
+    Output:
+        feature matrix, label, prediction window, and corresponding index.
+    Application note: In this function, swipeOutInactiveApp(...) is called with default parameters.
+    """
+    if windowSize is None: windowSize = slidingWindowSize
+    if featureSize is None: featureSize = featureTimeWindow - slidingWindowSize + 1
+    if predictSize is None: predictSize = predictTimeWindow - slidingWindowSize + 1
+    if topk is None: topk = top
+    featureEndTime = predictTimeStamp - windowSize + 1
+    featureStartTime = featureEndTime - featureSize
+    featureTotal = totalDataMatrix[:,featureStartTime:featureEndTime]
+    predictTotal = totalDataMatrix[:,predictTimeStamp:predictTimeStamp + predictSize]
+    featureMatrix, predictMatrix, remainingIndex = swipeOutInactiveApp(featureTotal, predictTotal)
+    eachWindowLabel = generateTopkLabelByCol(predictMatrix, topk)
+    consistentLabel = eachWindowLabel.sum(1) >= (predictSize - EPSILON)
+    firstFailLabel = eachWindowLabel[:,:failTime].sum(1) < EPSILON
+    lastSuccessLabel = eachWindowLabel[:,-successTime:].sum(1) > (successTime - EPSILON)
+    incrementalLabel = np.logical_and(firstFailLabel, lastSuccessLabel)
+    accumulateLabel = np.logical_or(incrementalLabel, consistentLabel)
+    #accumulateLabel = incrementalLabel
+    predictTimeCol = np.ones(remainingIndex.shape[0], dtype='int')[:,None]
+    if standardizeMethod is not None:
+        return (standardizeMethod(featureMatrix),
+                accumulateLabel[:,None],
+                standardizeMethod(predictMatrix),
+                np.hstack((remainingIndex, predictTimeCol)))
+    else:
+        return (featureMatrix,
+                accumulateLabel[:,None],
+                predictMatrix,
+                np.hstack((remainingIndex, predictTimeCol)))
+
+def generateFeatureMatrixAndLabel(totalDataMatrix, singleMethod=singlePredictTimeNew,
+        windowSize=None, featureWindow=None, predictWindow=None, success=None, normalizeFlag=False):
     """
     Function: generateFeatureMatrixAndLabel
         generate feature matrix and label from total matrix, both training set and testing set.
     Input:
         totalDataMatrix: the complete data matrix.
+        singleMethod: single time method.
         windowSize: size of sliding window.
         featureWindow: size of feature window.
         predictWindow: size of prediction window.
         success: win at least these sliding windows to label as successful.
+        normalizeFlag: whether normalize or not.
     Output:
-        (train, test), each one contains (matrix, accumulate label, sliding window label, predict window data).
-    Application note: singlePredictTime(...) is called, where some
+        (train, test), each one contains (matrix, accumulate label, sliding window label, predict window data, corresponding index).
+    Application note: singlePredictTime(...) is called by default parameter, where some
     default-parameter-function-call involve. Also, sample(...) is called
     by default parameter.
     """
@@ -201,23 +275,30 @@ def generateFeatureMatrixAndLabel(totalDataMatrix, windowSize=None,
     if success is None: success = successThreshold
     featureSize = featureWindow - windowSize + 1
     predictSize = predictWindow - windowSize + 1
-    train = [np.zeros((0, featureSize)), np.zeros((0,1)), np.zeros((0,1)), np.zeros((0, predictSize)), np.zeros((0,2),dtype='int')]
-    test = [np.zeros((0, featureSize)), np.zeros((0,1)), np.zeros((0,1)), np.zeros((0, predictSize)), np.zeros((0,2),dtype='int')]
+    train = []
+    test = []
     for predictTime in xrange(featureWindow, totalDataMatrix.shape[1] - predictSize):
-        dataSet = singlePredictTime(totalDataMatrix, predictTime,
-                windowSize, featureSize, predictSize, success)
-        singleTrain, singleTest = sample(dataSet, None, predictTime)
-        for i in range(len(train)):
-            train[i] = np.vstack((train[i],singleTrain[i]))
-            test[i] = np.vstack((test[i],singleTest[i]))
+        if normalizeFlag:
+            dataSet = singleMethod(totalDataMatrix, predictTime)
+        else:
+            dataSet = singleMethod(totalDataMatrix, predictTime, normalize)
+        singleTrain, singleTest = sample(dataSet, predictTime)
+        for i in xrange(len(singleTrain)):
+            if i >= len(train):
+                train.append(singleTrain[i].copy())
+                test.append(singleTest[i].copy())
+            else:
+                train[i] = np.vstack((train[i],singleTrain[i]))
+                test[i] = np.vstack((test[i],singleTest[i]))
     return train, test
 
-def buildMatrix(filename=None):
+def buildMatrix(filename=None,normalizeFlag=False):
     """
     Function: buildMatrix
         Wrapper for the whole procedure.
     Input:
         filename: input category file name.
+        normalizeFlag: whether normalize or not.
     Output:
         (train, test), each one contains (matrix, accumulate label, sliding window label).
     Application note: This is a wrapper function therefore have many
@@ -226,7 +307,7 @@ def buildMatrix(filename=None):
     if filename is None: filename = inputFile
     rawData = rawDataMatrix(inputFile)
     transformed = compressMatrix(rawData)
-    return generateFeatureMatrixAndLabel(transformed)
+    return generateFeatureMatrixAndLabel(transformed, normalizeFlag=normalizeFlag)
 
 def plotDownloadInflation(filename=None):
     """
@@ -283,5 +364,6 @@ def randomPlot(filename=None):
     plt.show()
 
 if __name__ == '__main__':
-    #train, test = buildMatrix()
+    trainNormalized, testNormalized = buildMatrix(normalizeFlag=True)
+    train, test = buildMatrix(normalizeFlag=False)
     pass
